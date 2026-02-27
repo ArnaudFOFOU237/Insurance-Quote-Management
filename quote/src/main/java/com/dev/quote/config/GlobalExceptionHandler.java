314409aa39118730f5d5232e9a8b5507894c862c
Fofou Fonzam <gui.a.fofou.fonzam@accenture.com>
2026-02-27 11:36:20 +0400

    feat:add java doc in GlobalException

--- quote/src/main/java/com/dev/quote/config/GlobalExceptionHandler.java
+++ quote/src/main/java/com/dev/quote/config/GlobalExceptionHandler.java
@@ -14,6 +14,17 @@ import java.time.Instant;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Handles exceptions of type {@link ResourceNotFoundException}.
+     * <p>
+     * This method is triggered whenever a {@link ResourceNotFoundException} is thrown in the application.
+     * It constructs a {@link ProblemDetail} object with the HTTP status code 404 (NOT_FOUND) and the exception's message.
+     * Additional details such as a title ("Ressource introuvable") and a timestamp are included in the response.
+     * </p>
+     *
+     * @param ex the {@link ResourceNotFoundException} that was thrown
+     * @return a {@link ProblemDetail} object containing details about the error
+     */
     @ExceptionHandler(ResourceNotFoundException.class)
     public ProblemDetail handleNotFound(ResourceNotFoundException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
@@ -27,6 +38,17 @@ public class GlobalExceptionHandler {
         return problemDetail;
     }
 
+    /**
+     * Handles exceptions of type {@link BadRequestException}.
+     * <p>
+     * This method is triggered whenever a {@link BadRequestException} is thrown in the application.
+     * It constructs a {@link ProblemDetail} object with the HTTP status code 400 (BAD_REQUEST) and the exception's message.
+     * Additional details such as a title ("Mauvaise requête") and a timestamp are included in the response.
+     * </p>
+     *
+     * @param ex the {@link BadRequestException} that was thrown
+     * @return a {@link ProblemDetail} object containing details about the error
+     */
     @ExceptionHandler(BadRequestException.class)
     public ProblemDetail handleBadRequestException(ResourceNotFoundException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
@@ -40,6 +62,17 @@ public class GlobalExceptionHandler {
         return problemDetail;
     }
 
+    /**
+     * Handles exceptions of type {@link InvalidInputException}.
+     * <p>
+     * This method is triggered whenever an {@link InvalidInputException} is thrown in the application.
+     * It constructs a {@link ProblemDetail} object with the HTTP status code 400 (BAD_REQUEST) and the exception's message.
+     * Additional details such as a title ("Requête invalide") and a timestamp are included in the response.
+     * </p>
+     *
+     * @param ex the {@link InvalidInputException} that was thrown
+     * @return a {@link ProblemDetail} object containing details about the error
+     */
     @ExceptionHandler(InvalidInputException.class)
     public ProblemDetail handleInvalidInputException(InvalidInputException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
@@ -53,6 +86,17 @@ public class GlobalExceptionHandler {
         return problemDetail;
     }
 
+    /**
+     * Handles exceptions of type {@link ServiceUnavailableException}.
+     * <p>
+     * This method is triggered whenever a {@link ServiceUnavailableException} is thrown in the application.
+     * It constructs a {@link ProblemDetail} object with the HTTP status code 503 (SERVICE_UNAVAILABLE) and the exception's message.
+     * Additional details such as a title ("Service temporairement indisponible") and a timestamp are included in the response.
+     * </p>
+     *
+     * @param ex the {@link ServiceUnavailableException} that was thrown
+     * @return a {@link ProblemDetail} object containing details about the error
+     */
     @ExceptionHandler(ServiceUnavailableException.class)
     public ProblemDetail handleServiceUnavailableException(ServiceUnavailableException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
